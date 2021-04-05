f4a45a2f902907f7864ff6c36df4f4a659705e0f
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-04-05 11:29:35 -0500

    Added Powershell startup files

--- Startup/ApplyStartup.ps1
+++ Startup/ApplyStartup.ps1
@@ -1,4 +1,15 @@
 # Automatically copy StartAHK.ps1 into the Startup folder
+# Import modules
+# Import-Module PSScheduledJob
+
+# Enable running unsigned scripts
+Set-ExecutionPolicy RemoteSigned
+
+# Variables
+$start_ahk_path = ".\StartAHK.ps1"
+$trigger = New-JobTrigger -AtLogOn
+
+Register-ScheduledJob -Trigger $trigger -FilePath $start_ahk_path -Name StartAHKScripts
 
 # Copy Powershell StartAHK.ps1 to startup folder
-Copy-Item ".\StartAHK.ps1" -Destination "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+# Copy-Item ".\StartAHK.ps1" -Destination "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
---
+++ Startup/startup.cmd
@@ -0,0 +1,6 @@
+:: From: https://stackoverflow.com/a/32189430/3721066
+@echo off
+set START_AHK_PATH=""
+
+PowerShell -Command "Set-ExecutionPolicy Unrestricted"
+PowerShell %START_AHK_PATH%
